0462b43447345d671cbe43749971709229d72473
iamyaswantheswar <iamyaswantheswar@gmail.com>
2025-07-31 15:13:57 +0000

    New Py script to handle all user administration task has been added ,remove and update user functionality has been added json errors are resolved

---
+++ src/Pyri/Userhandeling.py
@@ -0,0 +1,61 @@
+import json
+from pathlib import Path
+from cryptography.fernet import Fernet
+base_path = Path(__file__).parent.parent.parent  # Points to pyt-WMS directory
+
+# Load the saved key
+file_path = base_path / "data" / "secret.key"
+with open(file_path, "rb") as key_file:
+    key = key_file.read()
+
+# Load the encrypted password
+file_path = base_path / "data" / "encrypted_password.txt"
+with open(file_path, "rb") as f:
+    encrypted = f.read()
+def check_admin(adminpass):
+    # Decrypt
+        cipher = Fernet(key)
+        decrypted_password = cipher.decrypt(encrypted).decode()
+        if adminpass == decrypted_password:
+            return True    
+        return False
+def user_check(username):
+  file_path = base_path / "data" / "users.json"
+  with open(file_path, "r") as f:
+      data = json.load(f)
+  # Check if username already exists
+  for u in data["users"]:
+      if u["username"] == username:
+          return False
+  return True
+def add_user(username, passwd):
+          file_path = base_path / "data" / "users.json"
+          if user_check(username):  # Check if user already exists
+            with open(file_path, "r+") as g:
+                data = json.load(g)
+                data["users"].append({"username":username, "password": passwd})
+                g.seek(0)  # Go back to beginning of file
+                json.dump(data, g)
+                g.truncate()  # Remove any leftover content
+def remove_user(username):
+    file_path = base_path / "data" / "users.json"
+    with open(file_path, "r+") as f:
+        data = json.load(f)
+        for u in data["users"]:
+            if u["username"] == username:
+                data["users"].remove(u)
+                f.seek(0)  # Go back to beginning of file
+                json.dump(data, f)
+                f.truncate()  # Remove any leftover content
+                return True
+        return False
+def update_user(username, passwd):
+    file_path = base_path / "data" / "users.json"
+    with open(file_path, "r+") as f:
+        data = json.load(f)
+        for u in data["users"]:
+            if u["username"] == username:
+                remove_user(username)
+                add_user(username, passwd)
+                return True
+        return False    
--- src/Pyri/login.py
+++ src/Pyri/login.py
@@ -1,39 +1,19 @@
 import tkinter as ui
 from tkinter import messagebox
-import csv
 from pathlib import Path
 from cryptography.fernet import Fernet
-import json
+import Userhandeling as uh
 
 # Get the path to the current script
-
-base_path = Path(__file__).parent.parent.parent  # Points to pyt-WMS directory
-
-# Load the saved key
-file_path = base_path / "data" / "secret.key"
-with open(file_path, "rb") as key_file:
-    key = key_file.read()
-
-# Load the encrypted password
-file_path = base_path / "data" / "encrypted_password.txt"
-with open(file_path, "rb") as f:
-    encrypted = f.read()
-
-
-
-
 
 
 ##functions
 def Check_admin():
     isadmin = False
-    def get_passwd():
+
+    def check_passwd():
         nonlocal isadmin
-        # Decrypt
-        cipher = Fernet(key)
-        decrypted_password = cipher.decrypt(encrypted).decode()
-        if passwd.get() ==decrypted_password :
-            f.close
+        if uh.check_admin(passwd.get()):
             isadmin = True
             messagebox.showinfo("Success", "Welcome Admin")
             second.destroy()
@@ -52,7 +32,7 @@ def Check_admin():
     label.place(relx=0.5, rely=0.3, anchor=ui.CENTER)
     passwd = ui.Entry(second, width=20, font=("Times", 15), bd=2, show="*")
     passwd.place(relx=0.5, rely=0.5, anchor=ui.CENTER)
-    enter = ui.Button(second, text="Continue", command=get_passwd)
+    enter = ui.Button(second, text="Continue", command=check_passwd)
     enter.place(relx=0.5, rely=0.7, anchor=ui.CENTER)
     login.wait_window(second)
     #passwd.bind("<Return>", lambda event:get_passwd)
@@ -61,34 +41,20 @@ def Check_admin():
         return True
 
 
-def add_user():
-    def user_check():
-        file_path = base_path / "data" / "users.json"
-        with open(file_path, "r") as f:
-            data = json.loads(f)
-        # Check if username already exists
-        for user in data["users"]:
-            if user["username"] == username.get():
-                messagebox.showerror("Error", "User already exists")
-                return False
-        return True
+def add_user(i=None,b=None):
+
     def match_passwd():
         if passwd.get() == passwd2.get():
-            if user_check():  # Check if user already exists
-                file_path = base_path / "data" / "users.json"
-                with open(file_path, "r+") as g:
-                    data = json.load(g)
-                    data["users"].append({"username": username.get(), "password": passwd.get()})
-                    g.seek(0)  # Go back to beginning of file
-                    json.dump(data, g)
-                    g.truncate()  # Remove any leftover content
-                messagebox.showinfo("Success", "User added successfully")
-                a.destroy()
+            uh.add_user(username.get(), passwd.get())
+            messagebox.showinfo("Success", "User added successfully")
+            a.destroy()
         else:
             messagebox.showerror("Error", "Passwords do not match")
-            
+            a.destroy()
+            add_user()
+
     if Check_admin():
-        print("Admin logged in")
+        print("Admin logged in to add user")
         a = ui.Toplevel(login)
         a.title("Add User")
         a.geometry("500x500")
@@ -110,27 +76,70 @@ def add_user():
         enter = ui.Button(a, text="Add User", command=match_passwd)
         enter.place(relx=0.5, rely=0.7, anchor=ui.CENTER)
         #login.wait_window(a)
+
+
 #remove user
-j=''''def remove_user():
+def remove_user():
     if Check_admin():
-        print("Admin logged in")
-        def edit_file():
-            file_path= base_path / "data" / "Users.csv"
-            with open(file_path, "r") as f:
-                
-            
+        def remove():
+            if uh.remove_user(entry.get()):
+                messagebox.showinfo("Success", "User removed successfully")
+                a.destroy()
+            else:
+                messagebox.showerror("Error", "User not found")
+                a.destroy()
+        print("Admin logged in to remove user")
         a = ui.Toplevel(login)
         a.title("Remove User")
-        a.geometry("500x500")
-        #a.grab_set()
+        a.geometry("300x150")
+        a.configure(bg="white")
         a.focus_set()
-        lable=ui.Label(a, text="Enter username to delete", bg="white")
-        entry=ui.Entry(a, width=20, font=("Times", 15), bd=2)
+        lable = ui.Label(a, text="Enter username to delete", bg='white')
+        lable.place(relx=0.5, rely=0.3, anchor=ui.CENTER)
+        entry = ui.Entry(a, width=20, font=("Times", 15), bd=2)
         entry.place(relx=0.5, rely=0.45, anchor=ui.CENTER)
-        enter = ui.Button(a, text="Remove User", command=lambda:edit_file())'''''
+        enter = ui.Button(a,
+                          text="Remove User",
+                          command=lambda: remove())
+        enter.place(relx=0.5, rely=0.7, anchor=ui.CENTER)
 
-##Main window
 
+#update user
+def update_user():
+    if Check_admin():
+
+        def update():
+            if uh.update_user(entry.get(), entry2.get()):
+                messagebox.showinfo("Success", "User updated successfully")
+                a.destroy()
+            else:
+                ans=messagebox.askquestion("Error", "User not found Do you want to add this user ?")
+                if ans == "yes":
+                    uh.add_user(entry.get(), entry2.get())
+                    messagebox.showinfo("Success", "User added successfully")
+                    a.destroy()
+                else:
+                    a.destroy()
+
+        print("Admin logged in to update user")
+        a = ui.Toplevel(login)
+        a.title("Update User")
+        a.geometry("500x500")
+        a.configure(bg="white")
+        a.focus_set()
+        lable = ui.Label(a, text="Enter username to update", bg='white')
+        lable.place(relx=0.5, rely=0.3, anchor=ui.CENTER)
+        entry = ui.Entry(a, width=20, font=("Times", 15), bd=2)
+        entry.place(relx=0.5, rely=0.4, anchor=ui.CENTER)
+        lable2 = ui.Label(a, text="Enter new password", bg='white')
+        lable2.place(relx=0.5, rely=0.5, anchor=ui.CENTER)
+        entry2 = ui.Entry(a, width=20, font=("Times", 15), bd=2, show="*")
+        entry2.place(relx=0.5, rely=0.6, anchor=ui.CENTER)
+        enter = ui.Button(a, text="Update User", command=lambda: update())
+        enter.place(relx=0.5, rely=0.7, anchor=ui.CENTER)
+
+
+#login window
 login = ui.Tk()
 login.title("Login")
 login.attributes("-fullscreen", True)
@@ -143,8 +152,8 @@ login.config(menu=menu)
 Users = ui.Menu(menu, tearoff=0)
 menu.add_cascade(label="Administration", menu=Users)
 Users.add_command(label="Add User", command=add_user)
-#Users.add_command(label="Remove User", command=lambda:remove_user())
-#Users.add_command(label="Update User", command=lambda: update_user())
+Users.add_command(label="Remove User", command=lambda: remove_user())
+Users.add_command(label="Update User", command=lambda: update_user())
 #Users.add_command(label="List Users", command=lambda: list_user())
 #Exit=ui.Menu(menu,tearoff=0)
 #menu.add_cascade(label="Exit",menu=Exit)
