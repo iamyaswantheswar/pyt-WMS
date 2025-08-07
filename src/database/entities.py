b1fc7cdd0a56f96701f33b56c40db5e1aabca721
RohanR493 <216427614+RohanR493@users.noreply.github.com>
2025-08-07 19:28:28 +0530

    Added database and functions to add data the database

--- src/database/entities.py
+++ src/database/entities.py
@@ -1,7 +1,10 @@
 # This file contains the entities used in the database.
 # And also the functions for the entities
 # It will be imported by other files in the database.
+import csv
+from pathlib import Path
 
+base_path = Path(__file__).parent.parent.parent
 
 class Product:
 
@@ -12,6 +15,27 @@ class Product:
         self.dimensions = dimensions
         self.weight = weight
 
+    def add_product(self, sku, name, category, dimensions, weight):
+        self.sku = sku
+        self.name = name
+        self.category = category
+        self.dimensions = dimensions
+        self.weight = weight
+        data = [
+            [
+                self.sku,
+                self.name,
+                self.category,
+                self.dimensions,
+                self.weight,
+            ]
+        ]
+
+        with open(base_path / "data" / "database" / "products.csv", 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+
 class Location:
 
     def __init__(self, block, zone, aisle, rack, shelf):
@@ -20,6 +44,26 @@ class Location:
         self.aisle = aisle
         self.rack = rack
         self.shelf = shelf
+
+    def add_location(self, block, zone, aisle, rack, shelf):
+        self.block = block
+        self.zone = zone
+        self.aisle = aisle
+        self.rack = rack
+        self.shelf = shelf
+        data = [
+            [
+                self.block,
+                self.zone,
+                self.aisle,
+                self.rack,
+                self.shelf,
+            ]
+        ]
+
+        with open(base_path / "data" / "database" / "locations.csv", 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
 
 class Inventory:
 
@@ -37,6 +81,8 @@ class Inventory:
                 self.quantity += quantity
         except ValueError:
             raise ValueError("Quantity must be an integer")
+
+        
 
 
 
