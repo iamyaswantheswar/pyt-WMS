3cb417040e8c1ce2cb1251e8164a709d2274c99c
iamyaswantheswar <iamyaswantheswar@gmail.com>
2025-08-12 04:35:22 +0000

    created python files for each page in  page_elements folder

--- 1.py
+++
@@ -1,4 +0,0 @@
-import json
-with open("data/users.json", "r") as f:
-  data = json.load(f)
-print(data)
--- src/Pyri/home.py
+++ src/Pyri/home.py
@@ -12,36 +12,41 @@ class cmds:
 
     def dashboard_ui(self):
         self.dest()
+        print("user entered dashboard")
         self.lable_frame = ui.Frame(self.home, bg="red")
         self.lable_frame.pack(side="top", fill="both", expand=True)
-        lbl = ui.Label(self.lable_frame, text="welcome to Dashboard", bg="black", fg="white")
-        lbl.place(relx=0.1, rely=0.5, anchor="w")
+        lbl = ui.Label(self.lable_frame, text="welcome to Dashboard")
+        lbl.place(relx=0.5, rely=0.5, anchor="center")
 
     def inventory_ui(self):
         self.dest()
-        self.lable_frame = ui.Frame(self.home, width=500, height=700, bg="green")
-        self.lable_frame.place(relx=0.5, rely=0.5, anchor="center")
+        print("user entered inventory")
+        self.lable_frame = ui.Frame(self.home,bg="green")
+        self.lable_frame.pack(side="top", fill="both", expand=True)
         lbl = ui.Label(self.lable_frame, text="welcome to Inventory")
-        lbl.place(relx=0.9, rely=0.5, anchor="e")
+        lbl.place(relx=0.5, rely=0.5, anchor="center")
 
     def sales_ui(self):
         self.dest()
-        self.lable_frame = ui.Frame(self.home, width=200, height=500, bg="blue")
-        self.lable_frame.place(relx=0.5, rely=0.5, anchor="center")
+        print("user entered sales")
+        self.lable_frame = ui.Frame(self.home,bg="blue")
+        self.lable_frame.pack(side="top", fill="both", expand=True)
         lbl = ui.Label(self.lable_frame, text="welcome to Sales")
-        lbl.place(relx=0.5, rely=0.9, anchor="center")
+        lbl.place(relx=0.5, rely=0.5, anchor="center")
 
     def purchases_ui(self):
         self.dest()
-        self.lable_frame = ui.Frame(self.home, width=500, height=500, bg="blue")
-        self.lable_frame.place(relx=0.5, rely=0.5, anchor="center")
+        print("user entered purchases")
+        self.lable_frame = ui.Frame(self.home,bg="lightblue")
+        self.lable_frame.pack(side="top", fill="both", expand=True)
         lbl = ui.Label(self.lable_frame, text="welcome to Purchases")
         lbl.place(relx=0.5, rely=0.5, anchor="center")
 
     def demand_ui(self):
         self.dest()
-        self.lable_frame = ui.Frame(self.home, width=500, height=500, bg="blue")
-        self.lable_frame.place(relx=0.5, rely=0.5, anchor="center")
+        print("user entered demand")
+        self.lable_frame = ui.Frame(self.home,bg="lightgreen")
+        self.lable_frame.pack(side="top", fill="both", expand=True)
         lbl = ui.Label(self.lable_frame, text="welcome to Demand")
         lbl.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -53,14 +58,7 @@ class cmds:
 
 class topbarelements:
     def __init__(self, label, topbar, command):
-        self.button = ui.Button(topbar,
-                                text=label,
-                                bg="black",
-                                fg="white",
-                                bd=0,
-                                font=("Times", 10, "bold"),
-                                anchor="w",
-                                command=command)
+        self.button = ui.Button(topbar,text=label,bg="black",fg="white",bd=0,font=("Times", 10, "bold"),anchor="w",command=command)
         self.button.config(width=10, height=2, borderwidth=0, highlightthickness=0)
         self.button.bind("<Enter>", lambda e: self.button.config(bg="white", fg="black"))
         self.button.bind("<Leave>", lambda e: self.button.config(bg="black", fg="white"))
