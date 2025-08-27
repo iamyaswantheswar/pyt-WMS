611a2ad02a373e5ecd632b212e61d4e04e369325
RohanR493 <216427614+RohanR493@users.noreply.github.com>
2025-08-27 20:11:09 +0530

    Removed class Location. Added sales backend

--- src/database/inventory.py
+++ src/database/inventory.py
@@ -9,6 +9,10 @@ from pathlib import Path
 
 base_path = Path(__file__).parent.parent.parent
 
+def generate_location_id( block, zone, aisle, rack, shelf):
+    location_id = f"{block}-{zone}-{aisle}-{rack}-{shelf}"
+    return location_id
+
 class Product:
 
     def __init__(self, id, name = '', category = [], price = 0, dimensions = '', weight = 0, quantity_in_stock = 0, locations = [], new = False):
@@ -74,6 +78,9 @@ class Product:
         if WriteToCSV:
             self.write_to_csv()
 
+    def generate_stock_id(self,loc):
+        return f"{self.id}@{loc}"
+
     def update_location(self, location, WriteToCSV = False):
         self.locations = location
         if WriteToCSV:
@@ -99,51 +106,20 @@ class Product:
                         writer.writeheader()
                         writer.writerows(rows)
 
-
-class Location:
-
-    def __init__(self, block, zone, aisle, rack, shelf, location_id):
-        self.block = block
-        self.zone = zone
-        self.aisle = aisle
-        self.rack = rack
-        self.shelf = shelf
-        self.location_id = location_id
-
-    def generate_location_id(self, block, zone, aisle, rack, shelf):
-        self.location_id = f"{block}-{zone}-{aisle}-{rack}-{shelf}"
-        return self.location_id
-
-    def new_location(self, block, zone, aisle, rack, shelf):
-        self.block = block
-        self.zone = zone
-        self.aisle = aisle
-        self.rack = rack
-        self.shelf = shelf
-        self.location_id = self.generate_location_id(block, zone, aisle, rack, shelf)
-        data = [
-            [
-                self.block,
-                self.zone,
-                self.aisle,
-                self.rack,
-                self.shelf,
-                self.location_id
-            ]
-        ]
-
-        with open(base_path / "data" / "database" / "locations.csv", 'a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(data)
-
-
 class Stock:
 
-    def __init__(self, product, location, quantity, expiry):
+    def __init__(self, stock_id, location, product, quantity, expiry):
+        self.stock_id = stock_id
         self.product = product
         self.location = location
         self.quantity = quantity
         self.expiry = expiry
+
+    def backup_csv(self):
+        file_path = base_path / "data" / "database" / "stock.csv"
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        backup_name = f"{file_path.stem}_{timestamp}.csv"
+        shutil.copy2(file_path, f"{file_path.parent}/history/stock/{backup_name}")
 
     def increase_stock(self, quantity, WriteToCSV = False):
         try:
@@ -151,7 +127,9 @@ class Stock:
                 raise ValueError
             else:
                 self.quantity += quantity
-                self.product.update_quantity(quantity, WriteToCSV = WriteToCSV)
+                if WriteToCSV:
+                    self.write_to_csv()
+                    self.product.update_quantity(quantity, WriteToCSV = WriteToCSV)
         except ValueError:
             raise ValueError("Quantity must be a positive integer")
 
@@ -161,10 +139,28 @@ class Stock:
                 raise ValueError
             else:
                 self.quantity -= quantity
-                self.product.update_quantity(-quantity, WriteToCSV = WriteToCSV)
+                if WriteToCSV:
+                    self.write_to_csv()
+                    self.product.update_quantity(-quantity, WriteToCSV = WriteToCSV)
         except ValueError:
             raise ValueError("Quantity must be a positive integer")
 
+    def write_to_csv(self):
+        self.backup_csv()
+        with open(base_path / "data" / "database" / "stock.csv", 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            rows = list(reader)
+            for row in rows:
+                if row["stock_id"] == self.stock_id:
+                    row["stock_id"] = self.stock_id
+                    row["location"] = self.location
+                    row["product"] = self.product
+                    row["quantity"] = self.quantity
+                    row["expiry"] = self.expiry
+                    with open(base_path / "data" / "database" / "stock.csv", mode = 'w', newline='') as file2:
+                        writer = csv.DictWriter(file2, fieldnames = ["stock_id", "location", "product", "quantity", "expiry"])
+                        writer.writeheader()
+                        writer.writerows(rows)
 
 
 
