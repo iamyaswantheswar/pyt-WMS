f3d7ea6481fe83e6c05e90c4c29a658fe24375ce
iamyaswantheswar <iamyaswantheswar@gmail.com>
2025-09-05 00:04:06 +0530

    updated purchases csv

--- src/Pyri/page_elements/sales.py
+++ src/Pyri/page_elements/sales.py
@@ -11,20 +11,20 @@ base_path = Path(__file__).parent.parent.parent.parent
 class sales_elements:
         def sales_ele(self,frame,home):
                 frame.grid_columnconfigure(0,minsize=250)
-                self.new_sale_button=ui.Button(frame,text="Add customer",command = lambda : None ,width=30,font=("Arial",10,"bold"))
+                self.new_sale_button=ui.Button(frame,text="Add customer",command = lambda : None ,width=25,font=("Arial",10,"bold"))
                 self.new_sale_button.place(relx=0.02,rely=0.05)
                 
-                self.sale_new_cus_button=ui.Button(frame,text="New Sale",command = lambda : None,width=30,font=("Arial",10,"bold"))
+                self.sale_new_cus_button=ui.Button(frame,text="New Sale",command = lambda : None,width=25,font=("Arial",10,"bold"))
                 self.sale_new_cus_button.place(relx=0.02,rely=0.25)
                 
-                self.modify_sale_button=ui.Button(frame,text="Modify sale ",command = lambda : None,width=30,font=("Arial",10,"bold"))
+                self.modify_sale_button=ui.Button(frame,text="Modify sale ",command = lambda : None,width=25,font=("Arial",10,"bold"))
                 self.modify_sale_button.place(relx=0.02,rely=0.45)
                 
-                self.delete_sale_button=ui.Button(frame,text="Delete sale ",command = lambda : None,width=30,font=("Arial",10,"bold"))
+                self.delete_sale_button=ui.Button(frame,text="Delete sale ",command = lambda : None,width=25,font=("Arial",10,"bold"))
                 self.delete_sale_button.place(relx=0.02,rely=0.65)
                 
                 
-                self.view_history_button=ui.Button(frame,text="View sales history ",command = lambda : None ,width=30,font=("Arial",10,"bold"))
+                self.view_history_button=ui.Button(frame,text="View sales history ",command = lambda : None ,width=25,font=("Arial",10,"bold"))
                 self.view_history_button.place(relx=0.02,rely=0.85)
 
                 sales_elements.refresh_csv(self,frame,home)
